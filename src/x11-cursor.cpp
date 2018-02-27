9c5e1aa0f9482433e9f4dbd5449ae51931998f62
Christophe de Dinechin <dinechin@redhat.com>
2018-02-28 00:55:03 +0100

    Move the X11CursorUpdater and X11CursorThread classes in a separate file
    This makes it possible to remove X11 dependencies from the main agent file.
    
    Note: it may be unsafe to call XCloseDisplay from the destructor.
    Doing some experiments throwing exceptions, I noticed messages such as:
    
    [xcb] Unknown request in queue while dequeuing
    [xcb] Most likely this is a multi-threaded client and XInitThreads
          has not been called
    [xcb] Aborting, sorry about that.
    spice-streaming-agent: xcb_io.c:165: dequeue_pending_request:
    Assertion `!xcb_xlib_unknown_req_in_deq' failed.
    Aborted (core dumped)
    
    The stack trace looks like this:
    
        #1  0x00007ffff65cf4da in abort () from /lib64/libc.so.6
        #2  0x00007ffff65c5d67 in __assert_fail_base () from /lib64/libc.so.6
        #3  0x00007ffff65c5e12 in __assert_fail () from /lib64/libc.so.6
        #4  0x00007ffff76b6ecc in dequeue_pending_request () from /lib64/libX11.so.6
        #5  0x00007ffff76b7d20 in _XReply () from /lib64/libX11.so.6
        #6  0x00007ffff76b36cd in XSync () from /lib64/libX11.so.6
        #7  0x00007ffff769494e in XCloseDisplay () from /lib64/libX11.so.6
    
    This is due to (pre-existing) incorrect synchronization of the cursor
    thread on termination, which is fixed in a later patch in this series.

---
+++ src/x11-cursor.cpp
@@ -0,0 +1,65 @@
+/* X11 cursor transmission
+ *
+ * \copyright
+ * Copyright 2018 Red Hat Inc. All rights reserved.
+ */
+#include "x11-cursor.hpp"
+
+#include <syslog.h>
+
+namespace spice
+{
+namespace streaming_agent
+{
+
+X11CursorUpdater::X11CursorUpdater(Stream &stream)
+    : stream(stream),
+      display(XOpenDisplay(NULL))
+{
+    if (display == NULL) {
+        throw Error("failed to open display").syslog();
+    }
+}
+
+X11CursorUpdater::~X11CursorUpdater()
+{
+    XCloseDisplay(display);
+}
+
+void X11CursorUpdater::send_cursor_changes()
+{
+    unsigned long last_serial = 0;
+
+    int event_base, error_base;
+    if (!XFixesQueryExtension(display, &event_base, &error_base)) {
+        syslog(LOG_WARNING, "XFixesQueryExtension failed, not sending cursor changes\n");
+        return; // also terminates the X11CursorThread if that's how we were launched
+    }
+    Window rootwindow = DefaultRootWindow(display);
+    XFixesSelectCursorInput(display, rootwindow, XFixesDisplayCursorNotifyMask);
+
+    while (true) {
+        XEvent event;
+        XNextEvent(display, &event);
+        if (event.type != event_base + 1) {
+            continue;
+        }
+
+        XFixesCursorImage *cursor = XFixesGetCursorImage(display);
+        if (!cursor || cursor->cursor_serial == last_serial) {
+            continue;
+        }
+
+        last_serial = cursor->cursor_serial;
+        stream.send<X11CursorMessage>(cursor);
+    }
+}
+
+X11CursorThread::X11CursorThread(Stream &stream)
+    : updater(stream),
+      thread(record_cursor_changes, this)
+{
+    thread.detach();
+}
+
+}} // namespace spic::streaming_agent
